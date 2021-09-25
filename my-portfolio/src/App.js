6e98e8e212b3f6a86ae26c10083b2fde915239cd
Jane Lu <jyl.janelu@gmail.com>
2021-09-25 11:54:28 -0700

    app.js

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,69 @@
+import React from "react";
+import About from "./components/about";
+import Contact from "./components/contact";
+import Navbar from "./components/navbar";
+import Projects from "./components/projects";
+import Resume from "./components/resume";
+import Skills from "./components/skills";
+
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div className="layout">
+      <Navbar></Navbar>
+      <About></About>
+      <Projects></Projects>
+      <Resume></Resume>
+      <Skills></Skills>
+      <Contact></Contact>
+    </div>
+  );
+};
+
+//import logo from "./logo.svg";
+
+//JSX Javascript and XML
+
+//functional component
+//a function that returns HTML
+
+/*export const App = () => {
+  const name = "Jane Lu";
+  const projects = ["pdf 1", "pdf 2", "pdf 3"];
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <ul id="myId">
+        {projects.map((projects, index) => {
+          return (
+            <div key={index}>
+              <h1>Projects</h1>
+              <li id={index}>{projects}</li>
+            </div>
+          );
+        })}
+      </ul>
+    </>*/
+/*<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};*/
+
+export default App;
+//only can have 1 default export
---
+++ my-portfolio/src/components/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div className="about">
+      <h2 className="title">Jane Lu</h2>
+    </div>
+  );
+};
+
+export default About;
